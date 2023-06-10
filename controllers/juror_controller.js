5af20a84ac36887e7eac09b1a87947b97a164acf
Layla Gallez <layla.gallez@gmail.com>
2023-06-09 21:02:13 -0700

    post requests and routes for get juror and postpone juror

--- controllers/juror_controller.js
+++ controllers/juror_controller.js
@@ -24,4 +24,53 @@ exports.juror_getOne = async (req, res) => {
 
 }
 
+//Get juror by badge number and pin
+exports.juror_getSummonDetails = async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    try {
+      const { BadgeNumber, PinCode } = req.params;
+      const juror = await Juror.findOne({ BadgeNumber, PinCode });
+  
+      if (!juror) {
+        return res.status(404).json({ message: "Juror not found" });
+      }
+  
+      const { SummonsDate, MailingAddress, City, State, GroupNumber, CanPostpone } = juror;
+      res.json({ SummonsDate, MailingAddress, City, State, GroupNumber, CanPostpone });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
 
+  exports.juror_postponeSummon = async (req, res) => {
+    try {
+      const { BadgeNumber, PinCode, postponeDate } = req.params;
+      const juror = await Juror.findOne({ BadgeNumber, PinCode });
+  
+      if (!juror) {
+        return res.status(404).json({ message: "Juror not found" });
+      }
+  
+      if (!juror.CanPostpone) {
+        return res.status(403).json({ message: "Juror cannot postpone" });
+      }
+  
+      const sixWeeksFromSummonDate = new Date(juror.SummonsDate);
+      sixWeeksFromSummonDate.setDate(sixWeeksFromSummonDate.getDate() + 42); // Add 42 days (6 weeks)
+  
+      const postponeDateTime = new Date(postponeDate);
+      if (postponeDateTime > sixWeeksFromSummonDate) {
+        return res.status(400).json({ message: "Postpone date exceeds 6 weeks from summon date" });
+      }
+  
+      // Update the summonDate with the new postpone date
+      juror.SummonsDate = postponeDate;
+      juror.CanPostpone = false;
+      await juror.save();
+  
+      res.json({ message: "Summon date postponed successfully" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+  
--- routes/routes.js
+++ routes/routes.js
@@ -13,9 +13,12 @@ router.get('/getAll', juror_controller.juror_getAll);
 //Get by ID Method
 router.get('/getOne/:id', juror_controller.juror_getOne);
 
+// Get summon details
+router.get('/getSummonDetails/:BadgeNumber/:PinCode', juror_controller.juror_getSummonDetails);
 
-
-
+// Postpone the summon date
+router.put('/postponeSummon/:BadgeNumber/:PinCode/:postponeDate', juror_controller.juror_postponeSummon);
+  
 router.get('/hello', (req, res) => {
     // send back a JSON response
     res.set('Access-Control-Allow-Origin', '*');
