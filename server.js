bb99c6fcda3223bf5dd82557fc04489e9ea8de51
Yavik Kapadia <yavikkapadia@gmail.com>
2023-06-12 17:58:37 -0700

    Added ESLINTing and camelcasing

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const routes = require('./routes/routes');
-const cors = require('cors');
-const database = require('./utils/dbUtils');
+require('dotenv').config()
+const express = require('express')
+require('jsonwebtoken')
+const routes = require('./routes/routes')
+const cors = require('cors')
+require('./utils/db_utils')
 
-const app = express();
-
+const app = express()
 const options = {
-    origin: ['http://localhost:3001'],
+  origin: ['http://localhost:3001']
 }
-app.use(cors(options));
-app.use(express.json());
+app.use(cors(options))
+app.use(express.json())
 
-app.use('/api', routes);
+app.use('/api', routes)
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-});
+  console.log(`Server Started at ${3000}`)
+})
