5add16f4a509ce5b3cd5551e5b77ac7530873682
Yavik Kapadia <yavikkapadia@gmail.com>
2023-06-26 00:11:55 -0700

    Revert "Revert "[Breaking Changes] Implemented JSON Web Token and refactored server.js""

--- controllers/juror_controller.js
+++ controllers/juror_controller.js
@@ -1,8 +1,11 @@
 const JurorModel = require('../models/juror_model')
 require('express-async-errors')
 const CONSTANT = require('../constants/JUROR_CONSTANTS')
+const JWT = require('../utils/jwt_utils')
 
+// DEV NOTE: THIS IS A TEST FUNCTION
 exports.jurorGetAll = async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
   try {
     const jurors = await JurorModel.find().limit(CONSTANT.MAX_GET_ALL)
     res.json(jurors)
@@ -24,31 +27,62 @@ exports.jurorGetOne = async (req, res) => {
 exports.jurorLogin = async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
   try {
-    const foundJuror = await JurorModel.findOne({
-      BadgeNumber: req.body.BadgeNumber,
-      PinCode: req.body.PinCode
-    })
+    const foundJuror = await verifyCredentials(
+      req.body.BadgeNumber,
+      req.body.PinCode
+    )
     if (!foundJuror) {
-      res.status(404).json({ message: 'Juror not found' })
+      return res.status(401).json({ message: 'Invalid credentials' })
     }
 
-    const {
-      FirstName,
-      LastName,
-      BadgeNumber,
-      SummonsDate,
-      GroupNumber,
-      ReportingLocation,
-      CanPostpone
-    } = foundJuror
-    res.json({
-      FirstName,
-      LastName,
-      BadgeNumber,
-      SummonsDate,
-      GroupNumber,
-      ReportingLocation,
-      CanPostpone
+    const token = JWT.generateToken(foundJuror) // Corrected code
+    res.header('Authorization', token)
+    res.json({ token })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
+
+exports.verify = async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  JWT.verifyToken(req, res, () => {
+    res.status(200).json({ message: 'Valid token' })
+  })
+}
+
+exports.jurorSummonDetails = async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  try {
+    // check for valid token
+    JWT.verifyToken(req, res, async () => {
+      // Token is valid, continue with the code execution
+      const foundJuror = await JurorModel.findOne({
+        BadgeNumber: req.body.BadgeNumber
+      })
+
+      if (!foundJuror) {
+        return res.status(404).json({ message: 'Juror not found' })
+      }
+
+      const {
+        FirstName,
+        LastName,
+        BadgeNumber,
+        SummonsDate,
+        GroupNumber,
+        ReportingLocation,
+        CanPostpone
+      } = foundJuror
+
+      res.json({
+        FirstName,
+        LastName,
+        BadgeNumber,
+        SummonsDate,
+        GroupNumber,
+        ReportingLocation,
+        CanPostpone
+      })
     })
   } catch (err) {
     res.status(500).json({ message: err.message })
@@ -58,71 +92,110 @@ exports.jurorLogin = async (req, res) => {
 exports.jurorPostpone = async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
   try {
-    const foundJuror = await JurorModel.findOne({
-      BadgeNumber: req.body.BadgeNumber
-    })
-    if (!foundJuror) {
-      return res.status(404).json({ message: 'Juror not found' })
-    }
-    const newDate = new Date(req.body.PostponeDate + 'T00:00:00Z')
-    const serviceDate = new Date(foundJuror.SummonsDate + 'T00:00:00Z')
-
-    if (!foundJuror.CanPostpone) {
-      return res.status(404).json({ message: 'Juror cannot postpone' })
-    }
-
-    if (newDate.getDay() !== 0) {
-      return res.status(404).json({ message: 'Postpone date must be a Monday' })
-    }
-    if (newDate < serviceDate) {
-      return res
-        .status(404)
-        .json({ message: 'Postpone date must be after service date' })
-    }
-    if (newDate > serviceDate + CONSTANT.MAX_DAYS_TO_POSTPONE) {
-      return res.status(404).json({
-        message: 'Postpone date must be within 6 weeks of service date'
+    JWT.verifyToken(req, res, async () => {
+      // Token is valid, continue with the code execution
+      const foundJuror = await JurorModel.findOne({
+        BadgeNumber: req.body.BadgeNumber
       })
-    }
 
-    foundJuror.SummonsDate = req.body.PostponeDate
-    foundJuror.CanPostpone = false
-    const newJuror = await foundJuror.save()
-    const {
-      FirstName,
-      LastName,
-      BadgeNumber,
-      SummonsDate,
-      GroupNumber,
-      ReportingLocation,
-      CanPostpone
-    } = newJuror
-    res.json({
-      FirstName,
-      LastName,
-      BadgeNumber,
-      SummonsDate,
-      GroupNumber,
-      ReportingLocation,
-      CanPostpone
+      if (!foundJuror) {
+        return res.status(404).json({ message: 'Juror not found' })
+      }
+      console.log('postpone date:', req.body.PostponeDate)
+      const newDate = new Date(req.body.PostponeDate + 'T00:00:00Z')
+
+      const serviceDate = new Date(foundJuror.SummonsDate + 'T00:00:00Z')
+
+      console.log('newDate:', newDate)
+      console.log('day of the week:', newDate.getDay())
+
+      if (!foundJuror.CanPostpone) {
+        return res.status(404).json({ message: 'Juror cannot postpone' })
+      }
+
+      if (newDate.getDay() !== 1) {
+        // Adjusted condition to check for Monday (Monday corresponds to 1)
+        return res
+          .status(404)
+          .json({ message: 'Postpone date must be a Monday' })
+      }
+
+      if (newDate < serviceDate) {
+        return res
+          .status(404)
+          .json({ message: 'Postpone date must be after service date' })
+      }
+
+      if (newDate > serviceDate + CONSTANT.MAX_DAYS_TO_POSTPONE) {
+        return res.status(404).json({
+          message: 'Postpone date must be within 6 weeks of service date'
+        })
+      }
+
+      foundJuror.SummonsDate = req.body.PostponeDate
+      foundJuror.CanPostpone = false
+      const newJuror = await foundJuror.save()
+      const {
+        FirstName,
+        LastName,
+        BadgeNumber,
+        SummonsDate,
+        GroupNumber,
+        ReportingLocation,
+        CanPostpone
+      } = newJuror
+
+      res.json({
+        FirstName,
+        LastName,
+        BadgeNumber,
+        SummonsDate,
+        GroupNumber,
+        ReportingLocation,
+        CanPostpone
+      })
     })
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 }
+
 exports.jurorChangeCanPostpone = async (req, res) => {
   try {
-    const foundJuror = await JurorModel.findOne({
-      BadgeNumber: req.body.BadgeNumber
-    })
-    if (!foundJuror) {
-      res.status(404).json({ message: 'Juror not found' })
-    }
-    foundJuror.CanPostpone = req.body.CanPostpone
-    const newJuror = await foundJuror.save()
+    JWT.verifyToken(req, res, async () => {
+      // Token is valid, continue with the code execution
+      const foundJuror = await JurorModel.findOne({
+        BadgeNumber: req.body.BadgeNumber
+      })
+      if (!foundJuror) {
+        res.status(404).json({ message: 'Juror not found' })
+      }
+      foundJuror.CanPostpone = req.body.CanPostpone
+      const newJuror = await foundJuror.save()
 
-    res.json(newJuror)
+      res.json(newJuror)
+    })
   } catch (err) {
     res.status(500).json({ message: err.message })
+  }
+}
+
+// Helper function for verifying credentials and returning the juror
+const verifyCredentials = async (BadgeNumber, PinCode) => {
+  try {
+    const juror = await JurorModel.findOne({ BadgeNumber, PinCode })
+    if (!juror) {
+      return null
+    }
+
+    const isValidPinCode = juror.PinCode === PinCode
+
+    if (!isValidPinCode) {
+      return null
+    }
+
+    return juror
+  } catch (err) {
+    return null
   }
 }
