b82e2574ca4546655c9f4b8e401997b5f0bd4566
Yavik Kapadia <yavikkapadia@gmail.com>
2023-06-25 21:10:12 -0700

    Revert "[Breaking Changes] Implemented JSON Web Token and refactored server.js"

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const app = require('./app')
+require('dotenv').config()
+const express = require('express')
+require('jsonwebtoken')
+const routes = require('./routes/routes')
+const cors = require('cors')
+require('./utils/db_utils')
 
-const PORT = 3000
+const app = express()
+const options = {
+  origin: ['http://localhost:3001']
+}
+app.use(cors(options))
+app.use(express.json())
 
-app.listen(PORT, () => {
-  console.log(`Server Started at ${PORT}`)
+app.use('/api', routes)
+
+app.listen(3000, () => {
+  console.log(`Server Started at ${3000}`)
 })
