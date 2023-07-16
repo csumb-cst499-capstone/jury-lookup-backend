a035ff5a26393dad5b2a54a97114d53a71d4e64c
AlonsoVega <alvega@csumb.edu>
2023-07-16 15:31:56 -0700

    ignore this

--- logger/logger.js
+++ logger/logger.js
@@ -1,16 +1,37 @@
+// const DevelopmentLogger = require("./development_logger");
+// const ProductionLogger = require("./production_logger");
+// let logger = null;
+// if (process.env.NODE_ENV === "prod") {
+//   logger = ProductionLogger();
+// }
+
+// if (process.env.NODE_ENV === "dev") {
+//   logger = DevelopmentLogger();
+// }
+
+// if (process.env.NODE_ENV === "test") {
+//   logger = DevelopmentLogger();
+// }
+
+// module.exports = logger;
+
 const DevelopmentLogger = require("./development_logger");
 const ProductionLogger = require("./production_logger");
+
 let logger = null;
-if (process.env.NODE_ENV === "prod") {
+
+if (process.env.NODE_ENV === "production") {
   logger = ProductionLogger();
-}
-
-if (process.env.NODE_ENV === "dev") {
+} else if (process.env.NODE_ENV === "development") {
   logger = DevelopmentLogger();
-}
-
-if (process.env.NODE_ENV === "test") {
+} else if (process.env.NODE_ENV === "test") {
+  logger = DevelopmentLogger();
+} else {
+  // Default logger implementation in case NODE_ENV is not set
   logger = DevelopmentLogger();
 }
 
 module.exports = logger;
+
+
+//     set NODE_ENV=dev && nodemon server.js
