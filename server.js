3322e622fba32429af6edcce5d8f1b3bf63ae72b
Yavik Kapadia <yavikkapadia@gmail.com>
2023-07-20 00:14:10 -0700

    implemented auth0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const logger = require("./logger/logger");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
